d7c31fd6758ee27d931f076e001356d8e20bf996
gat41 <gloria.tengaihui@gmail.com>
2017-02-26 20:22:28 +0800

    add files to repo

---
+++ playground.rb
@@ -0,0 +1,68 @@
+# if you have not installed the needed gems, please do the following:
+# gem install sqlite3
+# gem install activerecord
+
+require "sqlite3"
+require "active_record"
+
+# this line configures ActiveRecord to connect to a sqlite3 database in the same folder named "students.sqlite3"
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "./students.sqlite3")
+
+# This sets up ActiveRecord to use Ruby OOP with SQL data
+class Student < ActiveRecord::Base
+end
+
+####################################
+# Write your test code here
+
+# 1. Select all students
+# pay attention to the object that is returned to you, is it a User object or Active Record relation object?
+
+# Why can't you do Student.name or Student.email?
+
+# 2. Select the first student
+# pay attention to the object that is returned to you, is it a User object or Active Record relation object?
+
+# 3. Select the last student
+# pay attention to the object that is returned to you, is it a User object or Active Record relation object?
+
+# 4. Use each to iterate through the #<ActiveRecord::Relation> object to display the name of each student
+
+
+# 5. Create a new user using new and save
+
+
+# 6. Create a new user using create
+
+# 7. Find student by the name Dr. Lois Pfeff using where.
+# pay attention to the object that is returned to you, is it the User object or Active Record relation object?
+
+# What will the following return to you? (remember to comment out the code!)
+# p student.id
+# p student.name
+# p student.email
+# p student.age
+
+
+# 8. Find user by name using find_by
+# pay attention to the object that is returned to you, is it the User object or Active Record relation object?
+
+# What will the following return to you?
+# p student.id
+# p student.name
+# p student.email
+# p student.age
+
+# Do research on the difference between "where" and "find_by".
+
+# 9. Find the student with id = 7 using find and find_by
+# pay attention to the object that is returned to you, is it the User object or Active Record relation object?
+
+
+# 10. Update information for student with id = 5 using student.email and save. Change the student's email to elsie@example.com
+
+# 11. Now use update to update this student's age to 21.
+
+# 12. Delete student with id 21 using delete
+
+# 13. Delete student with id 22 using destroy
